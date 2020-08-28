e375109c5244715540a5803e67833f5dcfb56c54
malharsb <52419960+malharsb@users.noreply.github.com>
2020-08-28 12:06:16 -0400

    Add files via upload
    Added create_video_from_tifs function

--- python/generate_videos.py
+++ python/generate_videos.py
@@ -5,9 +5,10 @@ import os
 from PIL import Image
 
 
-def sort_files(input_path, print_names=False):
+def sort_files(input_path, frmt='png', print_names=False):
     # load filenames
-    filenames = glob.glob(input_path+'*.png')
+    filenames = glob.glob(input_path+'*.'+frmt)
+    print("number of files: {}".format(len(filenames)))
     # custom sorting function
     filenames.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
     if print_names:
@@ -15,15 +16,12 @@ def sort_files(input_path, print_names=False):
             print(f)
     return filenames
 
-def create_video_from_pngs(filenames, output_path, video_name, frame_rate=10, frmt="mp4", is_rgb=False):
-    # extract image from filename and store as a numpy array
-    images = []
-    for f in filenames:
-        im = Image.open(f)
-        im = np.array(im)
-        images.append(im)
-    images = np.array(images)
-    print("Images shape: {}".format(images[0].shape))
+def load_tifs(path):
+    retval, mats = cv2.imreadmulti(path)
+    mats = np.array(mats)
+    return retval, mats
+
+def write_video(images, output_path, video_name, frame_rate, frmt, is_rgb):
     img_shape = images[0].shape[:2]
     print("Output video shape: {}".format(img_shape))
     # create videowriter
@@ -37,25 +35,30 @@ def create_video_from_pngs(filenames, output_path, video_name, frame_rate=10, fr
     out.release()
     print("Successfully created "+frmt+" video")
 
-# under construction
-def create_video_from_tifs(input_path, output_path):
-    mats = load_maxprojections(input_path, save=False)
-    print("mats shape: {}".format(mats.shape))
-    s = mats.shape[1]
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'MP4V'), 5, (s,s), 0)
-    for i in range(mats.shape[0]):
-        out.write(mats[i])
-    out.release()
-    print("Done")
+def create_video_from_pngs(filenames, output_path, video_name, frame_rate=10, frmt="mp4", is_rgb=False):
+    # extract image from filename and store as a numpy array
+    images = []
+    for f in filenames:
+        im = Image.open(f)
+        im = np.array(im)
+        images.append(im)
+    images = np.array(images)
+    print("Images shape: {}".format(images[0].shape))
+    write_video(images, output_path, video_name, frame_rate, frmt, is_rgb)
+
+def create_video_from_tifs(filenames, output_path, video_name, frame_rate=10, frmt="mp4", is_rgb=False):
+    images=[]
+    for f in filenames:
+        _, img = load_tifs(f)
+        images.append(np.squeeze(img))
+    images = np.array(images)
+    print(images.shape)
+    write_video(images, output_path, video_name, frame_rate, frmt, is_rgb)
 
 # under construction
-def create_video_from_mats(mats_path, video_path):
-    mats = np.load(mats_path)
-    print(mats.shape)
-    writer = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (512,512))
-    for i in range(mats.shape[0]):
-        writer.write(mats[i])
-    writer.release()
+def create_video_from_npy(filenames, output_path, video_name, frame_rate=10, frmt="mp4", is_rgb=False):
+    pass
+
     
 def convert_avi_to_mp4(avi_file_path, output_name):
     os.popen("ffmpeg -i '{input}' -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 '{output}.mp4'".format(input = avi_file_path, output = output_name))
@@ -65,21 +68,21 @@ def convert_avi_to_mp4(avi_file_path, output_name):
 if __name__ == '__main__':
 
     # set input path and output paths
-    input_path = '../data/pngs_rgb/'
+    input_path = '../data/tifs/'
     output_path = '../outputs/'
-    video_name = 'pngs_rgb'
+    video_name = 'tifs'
 
     # sort files
-    filenames = sort_files(input_path, print_names=False)
+    filenames = sort_files(input_path, frmt='tif', print_names=False)
 
     # select video creation operation
-    create_video_from_pngs(filenames, output_path, video_name, 10, frmt="avi", is_rgb=True)
-    # create_video_from_tifs(input_path, output_path)
-    # create_video_from_mats(mats_path, video_path)
+    # create_video_from_pngs(filenames, output_path, video_name, 10, frmt="avi", is_rgb=False)
+    # create_video_from_tifs(filenames, output_path, video_name, 10, frmt="mp4", is_rgb=False)
+    create_video_from_npy(filenames, output_path, video_name, 10, frmt="mp4", is_rgb=False)
     
     # convert AVI to MP4
-    convert = False
-    avi_file_path = '../outputs/pngs_rgb.avi'
+    convert = 0
+    avi_file_path = '../outputs/tifs.avi'
     mp4_video_name = video_name+'_mp4'
     if convert:    
         convert_avi_to_mp4(avi_file_path, output_path+mp4_video_name)
